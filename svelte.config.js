47c6961a6d4318a35f2910897ba60f6aca4e25e1
Khakers <22665282+khakers@users.noreply.github.com>
2025-04-01 13:47:42 -0700

    Add CSP directives and use git rev-parse for sveltekit app version

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import * as child_process from 'node:child_process';
+
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
+		csp: {
+			directives: {
+				'script-src': ['self'],
+				'media-src': ['self', "cdn.discordapp.com", "media.discordapp.com"],
+			},
+		},
+		version: {
+			name: child_process.execSync('git rev-parse HEAD').toString().trim()
+		}
 	}
 };
 
