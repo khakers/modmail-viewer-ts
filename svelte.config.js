68a932a7533916fbc46bd757aaeb06ecc04c2170
Khakers <22665282+khakers@users.noreply.github.com>
2026-01-19 22:04:00 -0800

    feat: bump dependencies and enable svelte experimental features

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import * as child_process from 'node:child_process';
 
-
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
@@ -19,6 +19,14 @@ const config = {
 		version: {
 			name: child_process.execSync('git rev-parse HEAD').toString().trim()
 		},
+		experimental: {
+			remoteFunctions: true
+		}
+	},
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
 	}
 };
 
