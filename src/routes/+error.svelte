32ab15c013860a9adc1d3fc83a77c7b181c4d569
Khakers <22665282+khakers@users.noreply.github.com>
2026-01-21 21:30:38 -0800

    feat: improve error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,8 +5,14 @@
 
 <div class="flex h-screen flex-col items-center justify-center">
 	<h1 class="text-2xl">{m.slimy_misty_bumblebee_loop()}</h1>
-	<h2 class="font-mono text-2xl">{page.status}</h2>
+	{#if page.status !== 200}
+		<h2 class="font-mono text-6xl">{page.status}</h2>
+	{/if}
 	<p>{page.error?.message}</p>
-	<h3 class="text-xl">{m.icy_yummy_panther_explore()}</h3>
-	<span><code class="font-mono">{page.error?.requestId}</code></span>
+	{#if page.error?.requestId}
+		<h3 class="text-xl">{m.icy_yummy_panther_explore()}</h3>
+		<span><code class="font-mono">{page.error?.requestId}</code></span>
+		{:else}
+		<h3>{m.plain_slimy_tern_bless()}</h3>
+	{/if}
 </div>
