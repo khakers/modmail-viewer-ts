f7080a1e9a67c6e841d47a5a6a52c1f6f139ae33
Khakers <22665282+khakers@users.noreply.github.com>
2026-04-11 10:24:13 -0700

    fix: polish thread sharing function

--- src/routes/(authenticated)/[[tenant]]/logs/[id]/sharing.remote.ts
+++ src/routes/(authenticated)/[[tenant]]/logs/[id]/sharing.remote.ts
@@ -3,14 +3,50 @@ import { db } from '$lib/server/db';
 import { z } from 'zod/v4';
 import { sharedThreads } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
-import { options } from 'marked';
-import { formSchema } from './schema';
 import { logger } from '$lib/logger';
 import { invalid, json } from '@sveltejs/kit';
 import { encodeBase64UUID } from '$lib/uuid-utils';
 import { shareThread as shareThreadFunc } from '$lib/server/thread-sharing';
 import { error } from '$lib/server/skUtils';
-import { isBefore } from 'date-fns';
+import { isAfter, isBefore } from 'date-fns';
+import { getPermisionOrdinal, type PermissionLevel } from '$lib/server/modmail/permissions';
+import { isoDateStringToDate } from '$lib/zod-helpers';
+
+// Check the user's permissions for the tenant and throw a 403 if they don't meet the required level
+async function permissionGuard(requiredLevel: PermissionLevel): Promise<void> {
+	const req = getRequestEvent();
+
+	if (!req.locals.Tenant) {
+		logger.error('Tenant parameter is missing');
+		error(404, 'No tenant found', req);
+	}
+
+	if (!req.locals.discordApiClient) {
+		logger.error('Discord API client not found in request locals');
+		error(500, 'Internal Server Error', req);
+	}
+
+	const discordRoles = await req.locals.discordApiClient.getGuildUserInfo(
+		req.locals.Tenant.guildId
+	);
+
+	const permissionLevel = await req.locals.Tenant.getPermissionsLevel(
+		discordRoles.roles || [],
+		req.locals.user.discordUserId
+	);
+
+	if (getPermisionOrdinal(permissionLevel ?? 'ANYONE') <= getPermisionOrdinal(requiredLevel)) {
+		logger.error(
+			{
+				userId: req.locals.user.discordUserId,
+				tenantId: req.locals.Tenant.id,
+				permissionLevel
+			},
+			'User did not have sufficient permissions to access this resource'
+		);
+		error(403, 'Forbidden');
+	}
+}
 
 export const getThreadShares = query(z.string(), async (threadId) => {
 	return await db.select().from(sharedThreads).where(eq(sharedThreads.threadId, threadId));
@@ -23,7 +59,12 @@ export const shareThread = form(
 		showInternalMessages: z.coerce.boolean<boolean>(),
 		showAnonymousSenderName: z.coerce.boolean<boolean>(),
 		showSystemMessages: z.coerce.boolean<boolean>(),
-		expiresAt: z.iso.date().min(Date.now(), 'Expiration must be in the future').optional()
+		expiresAt: isoDateStringToDate
+			.refine(
+				(date) => date === null || isAfter(date, new Date()),
+				'Expiration must be in the future'
+			)
+			.optional()
 	}),
 	async (data, issue) => {
 		const req = getRequestEvent();
@@ -35,6 +76,18 @@ export const shareThread = form(
 			);
 			error(401, 'Unauthorized');
 		}
+
+		// TODO Allow per tenant configuration of minimum permission level required to share a thread
+		try {
+			permissionGuard('SUPPORTER');
+		} catch (err) {
+			logger.error(
+				{ error: err instanceof Error ? err.message : String(err) },
+				'Permission guard threw an error'
+			);
+			error(403, 'Forbidden');
+		}
+
 		logger.debug({ data }, 'shareThread called'); // thread id
 
 		// sveltekit handles remote functions in a way that keeps auth expectations valid
@@ -43,7 +96,7 @@ export const shareThread = form(
 		//   "routeId": "/(authenticated)/[[tenant]]/logs/[id]",
 		//   "path": "/_app/remote/z2d76e/shareThread",
 		//   "pathParams": {
-		//     "tenant": "qsr",
+		//     "tenant": "demo",
 		//     "id": "80a9516fa1f1"
 		//   },
 
@@ -70,20 +123,20 @@ export const shareThread = form(
 			invalid('Invalid thread');
 		}
 
-		// const id = await shareThreadFunc(
-		// 	data.id,
-		// 	req.locals.Tenant.id,
-		// 	req.locals.user.discordUserId,
-		// 	{
-		// 		expiresAt: data.expiresAt ? new Date(data.expiresAt) : null,
-		// 		requireAuthentication: data.requireAuthentication,
-		// 		showInternalMessages: data.showInternalMessages,
-		// 		showAnonymousSenderName: data.showAnonymousSenderName,
-		// 		showSystemMessages: data.showSystemMessages
-		// 	}
-		// );
+		const id = await shareThreadFunc(
+			data.id,
+			req.locals.Tenant.id,
+			req.locals.user.discordUserId,
+			{
+				expiresAt: data.expiresAt ? new Date(data.expiresAt) : null,
+				requireAuthentication: data.requireAuthentication,
+				showInternalMessages: data.showInternalMessages,
+				showAnonymousSenderName: data.showAnonymousSenderName,
+				showSystemMessages: data.showSystemMessages
+			}
+		);
 		logger.debug({ form: data }, 'share submission');
-		const id = crypto.randomUUID();
+		// const id = crypto.randomUUID();
 
 		getThreadShares(data.id).refresh(); // prefetch updated shares
 
--- src/routes/(authenticated)/[[tenant]]/logs/[id]/thread-share-form.svelte
+++ src/routes/(authenticated)/[[tenant]]/logs/[id]/thread-share-form.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import { buttonVariants } from '$lib/components/ui/button/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { Checkbox } from '$lib/components/ui/checkbox/index.js';
 	import * as Field from '$lib/components/ui/field/index.js';
 	import * as Popover from '$lib/components/ui/popover/index.js';
 	import { ShareIcon } from '@lucide/svelte';
 
 	import { shareThread } from './sharing.remote';
-
+	
 	let { threadId }: { threadId: string } = $props();
 
 	let open = $state(false);
-
 
 	const {
 		requireAuthentication,
@@ -30,8 +28,9 @@
 		<div class="max-h-175 overflow-y-auto">
 			<form
 				{...shareThread.enhance(async ({ form, data, submit }) => {
-					console.log('WTKLSD:AJGKLSD');
 					try {
+						// TODO remove debug
+						// surface errors to the user
 						console.debug('Submitting share thread form');
 						console.debug(data);
 						await submit();
@@ -48,12 +47,11 @@
 						<Field.Legend>Sharing Options</Field.Legend>
 						<Field.Group>
 							<Field.Field orientation="horizontal">
-                                <!-- I'm using inputs instead of the Checkbox component because it causes an issue where there are different or detached form instances or values -->
+								<!-- I'm using inputs instead of the Checkbox component because it causes an issue where there are different or detached form instances or values -->
 								<input
 									{...requireAuthentication.as('checkbox')}
-									class="appearance-none  border-input dark:bg-input/30 checked:bg-primary checked:text-primary-foreground dark:checked:bg-primary checked:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive peer flex size-4 shrink-0 items-center justify-center rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 checked:before:"
+									class="border-input dark:bg-input/30 accent-accent-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive peer flex size-4 shrink-0 items-center justify-center rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50"
 								/>
-								<Checkbox {...requireAuthentication.as('checkbox')} />
 								<Field.Content>
 									<Field.Label>Require Authentication</Field.Label>
 									<Field.Description>
@@ -61,18 +59,16 @@
 										view the shared thread.
 									</Field.Description>
 								</Field.Content>
-								{#each shareThread.fields.requireAuthentication.issues() as item}
+								{#each shareThread.fields.requireAuthentication.issues() as item (item.path)}
 									<Field.Error>{item.message}</Field.Error>
 								{/each}
 							</Field.Field>
 							<Field.Field orientation="horizontal">
 								<input
 									{...shareThread.fields.showInternalMessages.as('checkbox')}
-									class="border-input dark:bg-input/30 accent-accent-foreground dark:checked:bg-primary checked:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive peer flex size-4 shrink-0 items-center justify-center rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50"
+									class="border-input dark:bg-input/30 accent-accent-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive peer flex size-4 shrink-0 items-center justify-center rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50"
 								/>
-								<Checkbox
-									{...shareThread.fields.showInternalMessages.as('checkbox')}
-								/>
+
 								<Field.Content>
 									<Field.Label>Show Internal Messages</Field.Label>
 									<Field.Description>
@@ -80,7 +76,7 @@
 										the channel that are not replies.
 									</Field.Description>
 								</Field.Content>
-								{#each shareThread.fields.showInternalMessages.issues() as item}
+								{#each shareThread.fields.showInternalMessages.issues() as item (item.path)}
 									<Field.Error>{item.message}</Field.Error>
 								{/each}
 							</Field.Field>
@@ -96,7 +92,7 @@
 										Show the names of senders who have been anonymized.
 									</Field.Description>
 								</Field.Content>
-								{#each shareThread.fields.showAnonymousSenderName.issues() as item}
+								{#each shareThread.fields.showAnonymousSenderName.issues() as item (item.path)}
 									<Field.Error>{item.message}</Field.Error>
 								{/each}
 							</Field.Field>
@@ -112,43 +108,34 @@
 										Show system messages in the shared thread.
 									</Field.Description>
 								</Field.Content>
-								{#each shareThread.fields.showSystemMessages.issues() as item}
+								{#each shareThread.fields.showSystemMessages.issues() as item (item.path)}
 									<Field.Error>{item.message}</Field.Error>
 								{/each}
 							</Field.Field>
 						</Field.Group>
 					</Field.Set>
 					<Field.Set>
-						<!-- TODO expiration not being set -->
 						<Field.Legend>Expiration</Field.Legend>
-						<!-- For reasons unknown, the expiresAt is not actually being sent in the form -->
-						<!-- {@const supplier = shareThread.fields.expiresAt.as('date')} -->
 						<input
 							{...shareThread.fields.expiresAt.as('date')}
 							min={new Date().toISOString().split('T')[0]}
 							class="bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border shadow-xs"
 						/>
-						<!-- <DateSelector
-							bind:expiresAt={
-								() =>
-									supplier.value
-                                        ? new Date(supplier.value)
-                                        : null,
-								(value) =>
-									supplier.value =
-                                        value instanceof Date ? value.toISOString() : ''
-							}
-						/> -->
-						<!-- <div>{supplier.value}</div> -->
-						{#each shareThread.fields.expiresAt.issues() as item}
+						{#each shareThread.fields.expiresAt.issues() as item (item.path)}
 							<Field.Error>{item.message}</Field.Error>
 						{/each}
 					</Field.Set>
 					<Field.Field orientation="horizontal">
 						<Button type="submit">Submit</Button>
 						<Button variant="outline" type="button" onclick={() => (open = false)}
-							>Cancel</Button
+							>
+							Cancel
+							</Button
 						>
+						<!-- <Spinner class="ms-auto" /> -->
+						{#if shareThread.pending}
+							<span>Creating share link...</span>
+						{/if}
 					</Field.Field>
 				</Field.Group>
 			</form>
@@ -157,10 +144,4 @@
 </Popover.Root>
 
 <style>
-    /* input[type="checkbox"]::before {
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
-        content: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNoZWNrLWljb24gbHVjaWRlLWNoZWNrIj48cGF0aCBkPSJNMjAgNiA5IDE3bC01LTUiLz48L3N2Zz4=");
-        position: absolute;
-        display: block;
-    } */
 </style>
